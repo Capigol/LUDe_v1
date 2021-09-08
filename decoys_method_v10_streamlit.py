4eb4cb67380ba4649cd83935c8b79bcc35e13436
Lucas Nicolas Alberca <86059300+Capigol@users.noreply.github.com>
2021-09-08 18:25:58 -0300

    Update decoys_method_v10_streamlit.py

--- decoys_method_v10_streamlit.py
+++ decoys_method_v10_streamlit.py
@@ -5,10 +5,7 @@ Created on Mon May 10 14:27:11 2021
 @author: Lucas
 """
 
-##### Decoys generator #####
-### This script identify and select molecules into ChEMBL27 database 
-### these are physicochemical similar and also topological/structural different to the uploaded compounds 
-
+##### LUDe #####
 
 # Needed packages
 from pathlib import Path
@@ -57,23 +54,24 @@ t1_start = process_time()
 st.write("""
 # LIDeB Tools - LUDe
 
-LUDe (LIDEB’s Useful Decoys) is WebApp that generates, from a set of active compounds, decoys (putative inactive compounds)
-which can be used to retrospectively validate virtual screening tool/protocols. Decoys are molecules that have not been tested
-against a molecular target of interest but due to their structural features are presumably not prone to bind the target with high affinity.
-LUDe finds decoys in a curated ChEMBL27 database which are paired with the known active compounds in relation to certain general
-physicochemical properties (e.g., molecular weight, log P, and others) but which are topologically different from the query compounds.
-LUDe is conceptually similar to the Directory of Useful Decoys enhanced, but additional filters have been serially implemented
-to assure the topological dissimilarity between the decoys and the active compounds.
+LUDe (LIDEB’s Useful Decoys) is a WebApp that generates, from a set of active compounds, decoys (putative inactive compounds)
+which can be used to retrospectively validate virtual screening tools/protocols. Decoys are molecules that have not been tested
+against a molecular target of interest but due to their structural features are presumably not prone to bind the target with
+high affinity. LUDe finds decoys in a curated ChEMBL27 database; these decoys are paired with the known active compounds in
+relation to certain general physicochemical properties (e.g., molecular weight, log P, and others) but are topologically different
+from the query compounds. LUDe is conceptually similar to the Directory of Useful Decoys enhanced, but additional filters have been
+serially implemented to assure the topological dissimilarity between the decoys and the active compounds used as input.
 
-In this Web App, decoys are obtained by four sequential steps:
+In this Web App, decoys are obtained through four sequential steps:
     
-- Searching molecules with similar physicochemical properties of the loaded molecules in a curated ChEMBL database.
-- Filtering the selected molecules by dissimilarity against each individual loaded molecule.
-- Randomly selecting a desired number of decoys for each individual loaded molecule.
-- Filtering the selected molecules by the dissimilarity against all the loaded molecules.
-Finally, you can download a file with your **decoys.**
+
+- Searching molecules with similar physicochemical properties of the input active molecules in a curated ChEMBL database.
+- Filtering the selected molecules by dissimilarity against each individual input molecule.
+- Randomly selecting a desired number of decoys for each individual input molecule.
+- Filtering the selected molecules by the dissimilarity against all the input molecules (set of active compounds used as query). The decoys will have low Tanimoto similarity with the input compounds, and also low Maximum Common Substructure (MCS) ratio and distinctive molecular frameworks (Murcko scaffold). All in all, these three serial filters assure that the decoys will have a different topology in relation to the active compound used as a query. Furthermore, dissimilarity to the remaining active compounds is also checked. Finally, you can download a file with your decoys.
 
 The next workflow summarizes the steps performed by this method:
+
 """)
 
 text = '''
